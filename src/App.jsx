6a0f05886a7ee95c90b6b2f82e15b68f1bbe582c
DevRahat <dev.rahatislam@gmail.com>
2024-03-09 12:52:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+  
+  // Normal Function
+  function handleClick(){
+    alert('button Clicked');
+  }
+
+  // Arrow function
+  const handleClick2=()=>{
+    alert('button 2 clicked');
+  }
+  const addToFive=(num)=>{
+    alert(num+5);
+  }
+
+  return (
+    <>
+      <h3>React Core Concepts</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Two</button>
+      <button onClick={()=>{alert('third click')}}>Click Third</button>
+      
+      <button onClick={()=>addToFive(3)}>Click Forth</button>
+    </>
+  )
+}
+
+export default App
